62bbac3e89abcd29efda7d1eda4fdea987d1d256
Swapnil Akulwar <91781600+swapya97@users.noreply.github.com>
2023-03-04 14:45:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Message from './component/Message';
+
+function App() {
+  return (
+    <div>
+      <Message text="Hello, world!" />
+    </div>
+  );
+}
+
+export default App;
